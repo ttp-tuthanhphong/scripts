722c0945de4d7c24426dc77dad11089cbe49044e
ttp-tuthanhphong <100012791+ttp-tuthanhphong@users.noreply.github.com>
2025-06-11 14:24:27 +0700

    Update build-php.sh

--- build-php.sh
+++ build-php.sh
@@ -1,9 +1,1 @@
-# Download và load aliases
-curl -O https://raw.githubusercontent.com/ttp-tuthanhphong/scripts/main/my_aliases.sh
-source my_aliases.sh
-
-# Hoặc load trực tiếp (không lưu file)
-source <(curl -sSL https://raw.githubusercontent.com/ttp-tuthanhphong/scripts/main/my_aliases.sh)
-
-# Sau đó sử dụng
-build-php
+alias build-php='curl -O https://raw.githubusercontent.com/ttp-tuthanhphong/scripts/main/build_php_webserver.sh && chmod +x build_php_webserver.sh && ./build_php_webserver.sh && rm -f build_php_webserver.sh && echo "✅ Script cleaned up!"'
